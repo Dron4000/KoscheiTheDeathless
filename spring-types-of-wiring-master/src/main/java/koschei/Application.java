b025ba08d9d65b0840a325806871cab81fbba03c
Dron4000 <jericho93@yandex.ru>
2021-10-09 22:25:40 +0300

    KoscheiTheDeathless

---
+++ spring-types-of-wiring-master/src/main/java/koschei/Application.java
@@ -0,0 +1,19 @@
+package koschei;
+
+import koschei.config.AppConfig;
+import koschei.models.Duck5;
+import koschei.models.Egg6;
+import koschei.models.Needle7;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        KoscheiTheDeathless koscheiTheDeathless = applicationContext.getBean(KoscheiTheDeathless.class);
+
+        System.out.println(koscheiTheDeathless.getRulesByDeth());
+
+    }
+}
